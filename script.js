52a7fae7fb7cef861e56c468ed1251d9ab9d0659
Tydah Ngep <145172268+NixxMew@users.noreply.github.com>
2023-10-12 14:54:16 +1100

    Wk11 Lect Start

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-let introSection = document.querySelector("#intro")
+// let introSection = document.querySelector("#intro")
 // introSection.style.background = "gray"
 
-introSection.className = "redback";
+// introSection.className = "redback";
 
-let heading = document.createElement("h1");
-let headingText = document.createTextNode("BigHead");
-heading.appendChild(headingText);
-document.body.appendChild(heading);
-
-let elements = document.querySelectorAll(".class");
-for(let i=0; i<elements.length; i++){
-    
-
-}
+// let heading = document.createElement("h1");
+// let headingText = document.createTextNode("What is it?");
+// heading.appendChild(headingText);
+// document.body.appendChild(heading);
